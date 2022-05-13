4ddb5f9e0863518998dc425e6e39f23b58eeced3
Melissa Guachun <1212malka@gmail.com>
2022-05-13 14:34:20 -0400

    filter user component

---
+++ src/Components/FilterUsers.js
@@ -0,0 +1,21 @@
+import { React, useState } from 'react'
+import data from "./UserData.json"
+
+function FilterUsers(props) {
+    const filteredData = data.filter((el) => {
+        if (props.input === '') {
+            return el;
+        } else {
+            return el.text.toLowerCase().includes(props.input)
+        }
+    })
+    return (
+        <ul>
+            {filteredData.map((item) => (
+                <li key={item.id}>{item.text}</li>
+            ))}
+        </ul>
+    )
+}
+
+export default FilterUsers
